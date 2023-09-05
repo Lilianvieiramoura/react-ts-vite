366771a75db18229b10653c952447191bbafb6a2
Lilianvieiramoura <107009862+Lilianvieiramoura@users.noreply.github.com>
2023-09-05 09:45:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Greeting from './Greeting';
+import LuckyNumbers from './LuckyNumbers'
+
+import './App.css';
+
+function App() {
+  return (
+    <> 
+    <Header />;
+    <Greeting />
+    <LuckyNumbers />
+    </>
+  ) 
+}
+
+export default App;
